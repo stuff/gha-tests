2139a365f265f8fd23256b3ceba3334be20e998b
Nicolas Challeil <nicolas.challeil@openclassrooms.com>
2022-02-18 15:02:24 +0100

    test

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
+console.log('ok');
+
 export function installServiceWorker() {
   // Check for browser support of service worker
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('service-worker.js')
-      .then(function(registration) {
+      .then(function (registration) {
         // Successful registration
         // console.log('Hooray. Registration successful, scope is:', registration.scope);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         // Failed registration, service worker won’t be installed
         // console.log('Whoops. Service worker registration failed, error:', error);
       });
